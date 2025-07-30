70fbdf8d4e18f26213b9b2a6bee7bd03d25a827f
JamesSwarge <james.swarge@gmail.com>
2025-07-30 11:55:28 +0530

    Added Delivery screen and map.

--- client/app/Delivery.js
+++ client/app/Delivery.js
@@ -1,10 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { featured } from '@/constants'
+import { useNavigation } from 'expo-router';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function Delivery() {
+  const restaurant = featured.restaurants[0];
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Delivery</Text>
+    <View className='flex-1'>
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        className='flex-1'
+        mapType='standard'
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.lng,
+          }}
+        />
+     </MapView>
     </View>
   )
 }
--- client/app/OrderPreparing.js
+++ client/app/OrderPreparing.js
@@ -1,19 +1,33 @@
 import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation } from 'expo-router'
+import LottieView from 'lottie-react-native';
+import { Dimensions } from 'react-native';
 
 export default function OrderPreparing() {
   const navigation = useNavigation();
-
+  const { width, height } = Dimensions.get('window');
+  
   useEffect(()=>{
-    // setTimeout(()=> {
-    //   navigation.navigate("Delivery")
-    // }, 3000)
+    setTimeout(()=> {
+      navigation.navigate("Delivery")
+    }, 3000)
   }, [])
 
   return (
-      <View className='flex-1 bg-white justify-center items-center'>
-        <Image source={require('@/assets/images/delivery-man.gif')} style={{ width: 200, height: 200 }} />
+      <View className='flex-1 bg-white justify-center items-center w-full'>
+        {/* <Image source={require('@/assets/images/delivery-man.gif')} style={{ width: 200, height: 200 }} /> */}
+        <LottieView
+          source={require('@/assets/images/delivery-man.json')}
+          autoPlay
+          loop
+          style={{
+            width: width * 1.2,
+            height: width * 1.2,
+            alignSelf: 'center',
+          }}
+          // className="w-[80%] h-[80%] self-center"
+        />
       </View>
   )
 }
